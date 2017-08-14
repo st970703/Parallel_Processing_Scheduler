ff47a50e97fb801c8900b572ed1f3c2989aa0670
st970703 <st970703@gmail.com>
2017-08-14 12:31:21 +1200

    addeed files back to fix git

---
+++ src/test/java/DAGImpTest.java
@@ -0,0 +1,32 @@
+import dummyClasses.DummyNode;
+import implementations.structures.DAGImp;
+import interfaces.structures.Node;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * This is a test suite for the DAGImp class.
+ */
+public class DAGImpTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void addNodesTest() {
+        DAGImp dag = new DAGImp();
+        DummyNode n1 = new DummyNode("node");
+
+        dag.add(n1);
+
+        List<Node> nodes = dag.getAllNodes();
+
+        assertEquals(nodes.get(0).getName(), "node");
+    }
+
+}
